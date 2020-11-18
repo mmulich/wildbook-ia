617dc247323c0129cefff46a7e0d6d0ef9d8240c
Michael Mulich <michael.mulich@gmail.com>
2020-11-18 00:18:08 -0800

    fixup! :skull: temporarily install hupper to facilitate code reload

--- devops/_config/entrypoint.sh
+++ devops/_config/entrypoint.sh
@@ -6,6 +6,7 @@ setup
 
 # XXX ...
 /virtualenv/env3/bin/pip install hupper watchdog
-export HUPPER_DEFAULT_MONITOR=hupper.watchdog.WatchdogFileMonitor
+# export HUPPER_DEFAULT_MONITOR=hupper.watchdog.WatchdogFileMonitor
+export HUPPER_DEFAULT_MONITOR=hupper.polling.PollingFileMonitor
 
 exec gosu ${HOST_USER}:${HOST_USER} $@
