8c3f25124a91e57627cc00b196c81a0875e8e4c0
Michael Mulich <michael.mulich@gmail.com>
2020-11-18 00:21:52 -0800

    Install sentry-sdk

--- devops/_config/entrypoint.sh
+++ devops/_config/entrypoint.sh
@@ -9,4 +9,7 @@ setup
 # export HUPPER_DEFAULT_MONITOR=hupper.watchdog.WatchdogFileMonitor
 export HUPPER_DEFAULT_MONITOR=hupper.polling.PollingFileMonitor
 
+# XXX Enable Sentry
+/virtualenv/env3/bin/pip install "sentry-sdk[flask]>=0.10.2"
+
 exec gosu ${HOST_USER}:${HOST_USER} $@
